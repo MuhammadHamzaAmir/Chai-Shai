db8a95f9888b43b3c8c091819aba27004b783909
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2023-01-04 22:44:54 +0500

    removed unused imports

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,7 +1,5 @@
 import 'package:chai_shai/screens/authenticate/authenticate.dart';
 import 'package:chai_shai/screens/home/home.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 import 'package:chai_shai/models/logged_in_user.dart';
